26dd0cfaa96a5eebbfdc5c4bbc9c8304821775b0
guilhermemaiawarren <guilherme.maia@warren.com.br>
2022-11-09 09:51:50 -0300

    merge sorted array

---
+++ challenges/easy/merge_sorted_array.dart
@@ -0,0 +1,9 @@
+void main() {
+  merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+}
+
+void merge(List<int> nums1, int m, List<int> nums2, int n) {
+  nums1.removeRange(m, m + n);
+  nums1.addAll(nums2);
+  nums1.sort();
+}
