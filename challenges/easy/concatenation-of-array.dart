37efa8fa6d94cdfff18c0377399e0efa37b30477
guilhermemaiawarren <guilherme.maia@warren.com.br>
2022-11-11 12:39:33 -0300

    refactor get concatenation

--- challenges/easy/concatenation-of-array.dart
+++ challenges/easy/concatenation-of-array.dart
@@ -4,11 +4,18 @@ void main() {
 
 List<int> getConcatenation(List<int> nums) {
   List<int> ans = [];
-  while (true) {
-    for (int number in nums) {
-      ans.add(number);
-    }
-    if (ans.length == nums.length * 2) break;
+  for (int i = 0; i < 2; i++) {
+    ans.addAll(nums);
   }
   return ans;
 }
+// List<int> getConcatenation(List<int> nums) {
+//   List<int> ans = [];
+//   while (true) {
+//     for (int number in nums) {
+//       ans.add(number);
+//     }
+//     if (ans.length == nums.length * 2) break;
+//   }
+//   return ans;
+// }
