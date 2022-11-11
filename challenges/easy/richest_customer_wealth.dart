124704f07200a6d1b862760631e9b0ba17a8c7a6
guilhermemaiawarren <guilherme.maia@warren.com.br>
2022-11-11 12:54:23 -0300

    richest customer wealth refactor

--- challenges/easy/richest_customer_wealth.dart
+++ challenges/easy/richest_customer_wealth.dart
@@ -3,8 +3,8 @@ void main() {}
 int maximumWealth(List<List<int>> accounts) {
   int maximumWealth = 0;
   for (List<int> customerWealth in accounts) {
-    int wealth = 0;
-    customerWealth.forEach((element) => wealth += element);
+    int wealth = customerWealth.fold(
+        0, (previousValue, element) => previousValue + element);
     if (maximumWealth < wealth) maximumWealth = wealth;
   }
   return maximumWealth;
