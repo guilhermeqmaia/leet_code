517ff80364450e202c3e7d740d7f4fe6a63494ac
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-01-16 19:44:24 -0300

    feat three sum

---
+++ challenges/medium/three_sum.dart
@@ -0,0 +1,35 @@
+class Solution {
+  List<List<int>> threeSum(List<int> nums) {
+    nums.sort();
+
+    List<List<int>> triplets = [];
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+        int left = i + 1;
+        int right = nums.length - 1;
+        int target = -nums[i];
+
+        while (left < right) {
+          if (nums[left] + nums[right] == target) {
+            triplets.add([nums[i], nums[left], nums[right]]);
+
+            while (left < right && nums[left] == nums[left + 1]) left++;
+            while (left < right && nums[right] == nums[right - 1]) right--;
+            left++;
+            right--;
+          } else if (nums[left] + nums[right] < target) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+    }
+    return triplets;
+  }
+}
+
+void main(List<String> args) {
+  print(Solution().threeSum([0, 0, 0, 0]));
+}
