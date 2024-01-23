074800bdfc48414783afa5264486da5ef353d0c7
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2024-01-22 23:39:12 -0300

    refactor valid anagram

--- challenges/01-easy/valid_anagram.dart
+++ challenges/01-easy/valid_anagram.dart
@@ -2,11 +2,28 @@ class Solution {
   bool isAnagram(String s, String t) {
     final sList = s.split('');
     final tList = t.split('');
+    Map<String, int> hashLetters = {};
+    if (sList.length != tList.length) return false;
 
-    sList.sort();
-    tList.sort();
+    for (String s in sList) {
+      final value = hashLetters[s];
+      if (value == null)
+        hashLetters[s] = 1;
+      else
+        hashLetters[s] = value + 1;
+    }
 
-    return sList.join() == tList.join();
+    for (String s in tList) {
+      final value = hashLetters[s];
+      if (value == null)
+        return false;
+      else if (value == 1)
+        hashLetters.remove(s);
+      else
+        hashLetters[s] = value - 1;
+    }
+
+    return hashLetters.isEmpty;
   }
 }
 
